86a8e397104b06bb4e990b91b389158d2fe7aa59
noone4318 <74759923+noone4318@users.noreply.github.com>
2020-11-25 14:41:45 +0600

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,11 +1,11 @@
 const db = require("quick.db");
 const config = require("./../config.json");
 exports.run = async(client, message, args) => {
-  if(message.author.id == config.owner) {
-    let user = client.users.get(args[0]);
+  if(message.author.id === config.owner) {
+    let user = await client.users.fetch(args[0]);
     if(!user) return message.channel.send(`Invalid user or id ._.`);
     
-    let fetched = db.fetch(`blacklist_${user.id}`)
+    let fetched = db.get(`blacklist_${user.id}`)
     
     if(!fetched) {
       db.set(`blacklist_${user.id}`, true)
